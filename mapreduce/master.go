5f398141a864e4f3f6ffedd4932ddedcb0a4cac4
Matheus Camargo <matheuscamargo@gmail.com>
2016-09-13 15:13:03 -0300

    Solution

--- mapreduce/master.go
+++ mapreduce/master.go
@@ -26,13 +26,9 @@ type Master struct {
 	workers      map[int]*RemoteWorker
 	totalWorkers int // Used to generate unique ids for new workers
 
-	idleWorkerChan   chan *RemoteWorker
-	failedWorkerChan chan *RemoteWorker
-
-	///////////////////////////////
-	// ADD EXTRA PROPERTIES HERE //
-	///////////////////////////////
-	// Fault Tolerance
+	idleWorkerChan      chan *RemoteWorker
+	failedWorkerChan    chan *RemoteWorker
+	failedOperationChan chan *Operation
 }
 
 type Operation struct {
@@ -48,6 +44,7 @@ func newMaster(address string) (master *Master) {
 	master.workers = make(map[int]*RemoteWorker, 0)
 	master.idleWorkerChan = make(chan *RemoteWorker, IDLE_WORKER_BUFFER)
 	master.failedWorkerChan = make(chan *RemoteWorker, IDLE_WORKER_BUFFER)
+	master.failedOperationChan = make(chan *Operation, RETRY_OPERATION_BUFFER)
 	master.totalWorkers = 0
 	return
 }
@@ -77,9 +74,12 @@ func (master *Master) acceptMultipleConnections() {
 
 // handleFailingWorkers will handle workers that fails during an operation.
 func (master *Master) handleFailingWorkers() {
-	/////////////////////////
-	// YOUR CODE GOES HERE //
-	/////////////////////////
+	for failedWorker := range master.failedWorkerChan {
+		log.Println("Removing worker ", failedWorker.id, " from master list.")
+		master.workersMutex.Lock()
+		delete(master.workers, failedWorker.id)
+		master.workersMutex.Unlock()
+	}
 }
 
 // Handle a single connection until it's done, then closes it.
--- mapreduce/master_scheduler.go
+++ mapreduce/master_scheduler.go
@@ -8,10 +8,6 @@ import (
 // Schedules map operations on remote workers. This will run until InputFilePathChan
 // is closed. If there is no worker available, it'll block.
 func (master *Master) schedule(task *Task, proc string, filePathChan chan string) int {
-	//////////////////////////////////
-	// YOU WANT TO MODIFY THIS CODE //
-	//////////////////////////////////
-
 	var (
 		wg        sync.WaitGroup
 		filePath  string
@@ -34,16 +30,25 @@ func (master *Master) schedule(task *Task, proc string, filePathChan chan string
 
 	wg.Wait()
 
+	for len(master.failedOperationChan) > 0 {
+		numberOfFailures := len(master.failedOperationChan)
+		for i := 0; i < numberOfFailures; i++ {
+			failedOperation := <-master.failedOperationChan
+			log.Printf("Operation %v %v failed. Retrying.",
+				failedOperation.proc, failedOperation.filePath)
+			worker = <-master.idleWorkerChan
+			wg.Add(1)
+			go master.runOperation(worker, failedOperation, &wg)
+		}
+		wg.Wait()
+	}
+
 	log.Printf("%vx %v operations completed\n", counter, proc)
 	return counter
 }
 
 // runOperation start a single operation on a RemoteWorker and wait for it to return or fail.
 func (master *Master) runOperation(remoteWorker *RemoteWorker, operation *Operation, wg *sync.WaitGroup) {
-	//////////////////////////////////
-	// YOU WANT TO MODIFY THIS CODE //
-	//////////////////////////////////
-
 	var (
 		err  error
 		args *RunArgs
@@ -58,6 +63,7 @@ func (master *Master) runOperation(remoteWorker *RemoteWorker, operation *Operat
 		log.Printf("Operation %v '%v' Failed. Error: %v\n", operation.proc, operation.id, err)
 		wg.Done()
 		master.failedWorkerChan <- remoteWorker
+		master.failedOperationChan <- operation
 	} else {
 		wg.Done()
 		master.idleWorkerChan <- remoteWorker
